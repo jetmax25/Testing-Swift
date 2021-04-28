0b14bb737c57046dc5c427a5b86db44510cc74a2
Mike <jetmax25@gmail.com>
2021-04-28 12:03:55 -0400

    Init

---
+++ Shared/TestingSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingSwiftApp.swift
+//  Shared
+//
+//  Created by Michael Isasi on 4/28/21.
+//
+
+import SwiftUI
+
+@main
+struct TestingSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
